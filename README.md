bb1d1f6993b21bf67212f786010abee69bc01265
Nicholas C. Zakas <nicholas@nczconsulting.com>
2020-10-08 13:26:14 -0700

    Update: Switch IP address to listen on based on environment

--- README.md
+++ README.md
@@ -22,11 +22,12 @@ To start the server, run:
 $ npm start
 ```
 
-Locally, this will run the server on http://localhost:8080.
+Locally, this will run the server on http://127.0.0.1:8080.
 
 ## Notes
 
 1. The server reads from the `PORT` environment variable to determine which port to listen on. This is important because Google Cloud Run defines this variable and it's the only port that will be open.
+1. Google Cloud Run also requires that the server listen on 0.0.0.0 because 127.0.0.1 is a loopback address with no external access. I make this change by looking for the `K_SERVICE` environment variable that Cloud Run defines and then switching the address to 0.0.0.0.
 1. You no longer need to define a `Dockerfile` to run Node.js projects on Google Cloud Run. You can instead use the built-in [Node.js Buildpack](https://github.com/GoogleCloudPlatform/buildpacks). All you need to do is define `npm start`.
 
 ## License
--- src/index.js
+++ src/index.js
@@ -13,7 +13,15 @@ const fastify = require('fastify')({ logger: true })
 // Setup
 //-----------------------------------------------------------------------------
 
-const { PORT=8080 } = process.env;
+const { PORT=8080, K_SERVICE } = process.env;
+
+/*
+ * Cloud Run exposes a K_SERVICE environment variable. So, I'm using that to
+ * determine when the server is running in Cloud Run vs. running in a dev
+ * environment. Cloud Run needs to listen on 0.0.0.0 but in a dev environment
+ * it's a lot to easier to work with 127.0.0.1.
+ */
+const HOST = K_SERVICE ? "0.0.0.0" : "127.0.0.1";
 
 //-----------------------------------------------------------------------------
 // Server
@@ -26,8 +34,11 @@ fastify.get("/", async (request, reply) => {
 
 (async () => {
     try {
-        await fastify.listen(PORT, "0.0.0.0");
+        await fastify.listen(PORT, HOST);
         fastify.log.info(`Server listening on ${fastify.server.address().port}`);
+        if (K_SERVICE) {
+            fastify.log.info(`Running on service ${ K_SERVICE }`);
+        }
     } catch (error) {
         fastify.log.error(error)
         process.exit(1)
